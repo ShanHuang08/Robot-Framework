7c711da5d7c81d8b42c2aad7d6fadc7a11d96575
Shan Huang <shan800208@gmail.com>
2025-09-04 12:18:04 +0800

    0904_update

--- locustfiile.py
+++ locustfiile.py
@@ -1,35 +1,52 @@
 from locust import HttpUser, task, between, events
-from logging import info
+from logging import info, error, warning
 
 class ExternalApiUser(HttpUser):
     wait_time = between(1, 2)
     host = "https://pokeapi.co" # 設定host
+    api_v2 = "/api/v2/pokemon/"
 
 
     def url_connection(self, url):
         """Check if url is reachable, return True/False"""
-        import time
+        import requests, time
         for _ in range(3):
-            res = self.client.get(url)
+            res = requests.get(url) # requests.get() won't be recognized by locust
             if res.status_code == 200:
                 info(f"{url} Connection established.")
                 return
             time.sleep(2)
-        info(f"Cannot connect to {url}\nStatus code: {res.status_code}\nResponse: {res.text}")
+        error(f"Cannot connect to {url}\nStatus code: {res.status_code}\nResponse: {res.text}")
+    
+    def api_judgement(self, api, response, max_response_time=500):
+        """For `request()`. Check if response is valid, return True/False"""
+        if response.status_code != 200:
+            error(f"{api} Status code is not 200 but {response.status_code}")
+            return False
+        elif response.elapsed.total_seconds() * 1000 > max_response_time:
+            warning(f"{api} response time: {response.elapsed.total_seconds()*1000}ms > {max_response_time}ms.")
+            return False
+        return True
                 
+    def get_pokemon_apis(self, amount:int):
+        """Utilize `get()` tp test pokemon APIs"""
+        self.url_connection(self.host + self.api_v2)
+  
+        for i in range(0, amount):
+            api = self.api_v2 if i == 0 else f"{self.api_v2}{i}"
+            res = self.client.get(api)
+            if res.status_code != 200:
+                error(f"Status code is not 200 but {res.status_code}")
+    
     def test_pokemon_apis(self, amount:int):
-        """Test pokemon APIs"""
-        self.url_connection(self.host)
-        api1 = self.client.get("/api/v2/pokemon/")
-        if api1.status_code != 200:
-            api1.failure(f"Status code is not 200 but {api1.status_code}")
-        # else: api1.success() # Not support manual success, get() auto handle it.
-        # api1.elapsed.total_seconds()
+        """Utilize `request()` tp test pokemon APIs"""
+        self.url_connection(self.host + self.api_v2)
 
-        for i in range(1, amount):
-            apis = self.client.get(f"/api/v2/pokemon/{i}")
-            if apis.status_code != 200:
-                info(f"Status code is not 200 but {apis.status_code}")
+        for i in range(0, amount):
+            api = self.api_v2 if i == 0 else f"{self.api_v2}{i}"
+            with self.client.request("GET", api, catch_response=True) as response:
+                response.failure(f"API {api} test failed") if not self.api_judgement(api, response, 150) else response.success()
+
 
     @task
     def task_pokemon_one(self):
